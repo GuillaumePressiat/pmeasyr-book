160d0385ead76dc3049bebf275f29bf51f95fd09
GuillaumePressiat <guillaume.pressiat@aphp.fr>
2019-01-22 11:39:55 +0100

    Ajouts css

---
+++ _render.R
@@ -0,0 +1,5 @@
+
+my_output = 'html'
+bookdown::render_book('index.Rmd', output_format = "bookdown::gitbook")
+bookdown::render_book('index.rmd', output_format = 'bookdown::pdf_book')
+bookdown::render_book('index.rmd', output_format = 'bookdown::epub_book')
